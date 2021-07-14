b92eaa9d88e1fbb5c7a09b32bf9ccd8a0e972bb2
Costin Iordache <costin.iordache@five.ai>
2021-07-14 14:41:32 +0100

    revert unnecessary formatting change to ROS source file

--- clients/roscpp/include/ros/subscriber.h
+++ clients/roscpp/include/ros/subscriber.h
@@ -100,10 +100,10 @@ private:
     void unsubscribe();
     bool isValid() const;
 
-    std::string                   topic_;
-    NodeHandlePtr                 node_handle_;
+    std::string topic_;
+    NodeHandlePtr node_handle_;
     SubscriptionCallbackHelperPtr helper_;
-    bool                          unsubscribed_;
+    bool unsubscribed_;
   };
   typedef boost::shared_ptr<Impl> ImplPtr;
   typedef boost::weak_ptr<Impl> ImplWPtr;
