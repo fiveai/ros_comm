d58cbde61723f12c8c0f2eaeffa931bc364783db
Costin Iordache <costin.iordache@five.ai>
2021-07-14 12:48:59 +0100

    removed unused type

--- clients/roscpp/include/util/Types.h
+++ clients/roscpp/include/util/Types.h
@@ -15,7 +15,5 @@ namespace fiveai { namespace platform { namespace util
     using ByteCount = std::uint64_t;
     using PixelCount = std::uint64_t;
 
-    // Note, SizeBytes does not make sense!
-    using SizeElements = RectangleSize<ElementCount>;
     using SizePixels = RectangleSize<PixelCount>;
 }}}
