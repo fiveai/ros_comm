b26a8a6f1a5dfab883667f3138ee993b44004cee
robbuckley <20515024+robbuckley@users.noreply.github.com>
2021-08-18 10:27:57 +0100

    update copyright blocks in source files, and link to paper  (#6)
    * add link to paper on axiv
    
    * clarify copyright notices on new files
    
    Co-authored-by: Rob Buckley <robert.buckley@five.ai>

--- clients/roscpp/src/libros/threading/Utils.cpp
+++ clients/roscpp/src/libros/threading/Utils.cpp
@@ -1,6 +1,9 @@
 /***************************************************************************************************
- * Copyright Five AI 2021.
- * All rights reserved.
+ * Copyright Five AI Limited 2021
+ * All rights reserved
+
+ * This file is released under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+ * International License. Please refer to the README.md file supplied with this repository.
  ***************************************************************************************************/
 
 #include "threading/Utils.h"
